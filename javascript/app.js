ce93bf705075369abf5c709a0380bf50254e0a2a
Rosemari Burton <rosemariburtonCB5@MacBook-Air-2.local>
2019-07-26 06:54:06 -0400

    created my portfolio project

---
+++ javascript/app.js
@@ -0,0 +1,28 @@
+// Portfolio
+getCurrentYear = () => {
+    var _date = new Date()
+    var _year = _date.getFullYear()
+    document.getElementById("legal").innerHTML = `Copyright \u00A9 ${_year} Rosemari Jemison Burton`
+}
+
+$(window).on("load", function(){
+    getCurrentYear()
+});
+
+
+whatIdo = () => {
+    // Front-End
+    
+    //.socialItem img:hover {
+    //  height: 64px;
+    //  width: 64px;
+    //}
+
+    // <iframe src="https://giphy.com/embed/xUNda1t8JRgpyoC6re" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/animation-light-rain-xUNda1t8JRgpyoC6re">via GIPHY</a></p>
+
+    // Back-End
+
+
+
+    
+}
